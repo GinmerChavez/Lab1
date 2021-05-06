39dd56a2e5309e44e1d06675e2c8f9ae34ddc186
Ginmer Chavez <GinmerChavez@outlook.es>
2021-05-06 16:46:35 -0600

    Destructor- Virtuales- override

--- Lab01Inheritance.cpp
+++ Lab01Inheritance.cpp
@@ -1,18 +1,29 @@
 #include <iostream>  // allows program to output data to the screen
-
+using namespace std;
 struct Base {
-    virtual void do_something() {}
-};
-
-struct Derived1 : Base {
-    virtual void do_semothing() {
-        std::cout << "Derived1!!!" << std::endl;
+    virtual void do_something() const {
+        cout<<"PADRE"<<endl;
+    }
+    virtual ~Base() {
+        cout<<"Delete base"<<endl;
     }
 };
 
+struct Derived1 : Base {
+     void do_something()const override{
+        std::cout << "Derived1!!!" << std::endl;
+    }
+    ~Derived1(){
+         cout<<"Delete derived1"<<endl;
+     }
+};
+
 struct Derived2 : Base {
-    virtual void do_semothing() {
+    void do_something() const override {
         std::cout << "Derived2!!!" << std::endl;
+    }
+    ~Derived2(){
+        cout<<"Delete derived2"<<endl;
     }
 };
 
@@ -20,10 +31,15 @@ struct Derived2 : Base {
 int main(int argc, const char *argv[]) {
     std::cout << "Welcome to the UNA!" << std::endl;
 
-    Derived1 derived1;
-    derived1.do_semothing();
+    Base* padre= new Base();
+    padre->do_something();
+    delete padre;
+    Base * derived1= new Derived1();
+    derived1->do_something();
 
-    Derived2 derived2;
-    derived2.do_semothing();
+    Base* derived2= new Derived2();
+    derived2->do_something();
 
+    delete derived1;
+    delete derived2;
 }  // end function main
